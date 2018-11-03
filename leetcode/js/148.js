0cab2abce7337613af50b99cda2d808f85a3cf32
lybenson <naivebenson@gmail.com>
2018-11-03 21:30:44 +0800

    148

--- leetcode/js/148.js
+++ leetcode/js/148.js
@@ -14,5 +14,21 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-  
+  if (!head || !head.next) 
+    return head
+
+  let nodes = []
+  while (head) {
+    let node = new ListNode(head.val)
+    nodes.push(node)
+    head = head.next
+  }
+  nodes.sort(function(a, b) {
+    return a.val - b.val
+  })
+  let len = nodes.length
+  for (let i = 0; i < len - 1; i++)
+    nodes[i].next = nodes[i + 1]
+
+  return nodes[0]
 }
