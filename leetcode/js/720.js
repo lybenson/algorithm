9a7038cf221c940f603b9cb6df459d249de81a8b
lybenson <naivebenson@gmail.com>
2018-12-06 00:34:40 +0800

    delete 745

---
+++ leetcode/js/720.js
@@ -0,0 +1,13 @@
+/**
+ * 给出一个字符串数组words组成的一本英语词典。从中找出最长的一个单词，该单词是由words词典中其他单词逐步添加一个字母组成。若其中有多个可行的答案，则返回答案中字典序最小的单词。
+
+若无答案，则返回空字符串。
+ */
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestWord = function(words) {
+  
+};
