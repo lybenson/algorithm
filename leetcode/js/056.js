0d297d24f79cfe450ed595b5c44cc6fe7e1c9df5
lybenson <naivebenson@gmail.com>
2018-12-15 23:43:50 +0800

    56

---
+++ leetcode/js/056.js
@@ -0,0 +1,23 @@
+/**
+ * 给出一个区间的集合，请合并所有重叠的区间
+
+输入: [[1,3],[2,6],[8,10],[15,18]]
+输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6]
+
+ */
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
+var merge = function(intervals) {
+  
+};
