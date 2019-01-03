6315faf4ec95b590b49bca0532db04d11d005c07
lybenson <naivebenson@gmail.com>
2019-01-03 23:22:38 +0800

    21

---
+++ leetcode/js/021.js
@@ -0,0 +1,27 @@
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (l1 === null && l2 === null) return null
+  if (l1 === null) return l2
+  if (l2 === null) return l1
+  if (l1.val >= l2.val) {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
+  } else {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  }
+}
