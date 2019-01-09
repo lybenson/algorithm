cb63f92114c522379c8f90499fa63cef8ebe4685
lybenson <naivebenson@gmail.com>
2019-01-09 23:53:49 +0800

    088

---
+++ leetcode/js/088.js
@@ -0,0 +1,20 @@
+/**
+给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  nums1.length = m
+  nums2.length = n
+
+  Array.prototype.push.apply(nums1, nums2)
+  
+  nums1.sort((a, b) => {
+    return a - b
+  })
+}
