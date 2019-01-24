f642ca9755c3e9c15b4727000b729204dd4ebf5d
lybenson <naivebenson@gmail.com>
2019-01-25 01:16:27 +0800

    400

--- leetcode/js/400.js
+++ leetcode/js/400.js
@@ -7,5 +7,15 @@
  */
 var findNthDigit = function(n) {
   if (n < 10) return n
-  
+  let digit = 1
+  while (n > digit * 9 * 10 ** (digit - 1)) {
+    n -= digit * 9 * 10 ** (digit - 1)
+    digit += 1
+  }
+  let a = parseInt((n - 1) / digit)
+  let b = parseInt((n - 1) % digit)
+  let num = 10 ** (digit - 1) + a
+  return parseInt(String(num)[b])
 };
+
+console.log(findNthDigit(187))
