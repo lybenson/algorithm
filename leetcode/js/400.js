d93061948b98fc19cc04621605165e05556d1ad6
lybenson <naivebenson@gmail.com>
2019-01-24 23:46:14 +0800

    400

---
+++ leetcode/js/400.js
@@ -0,0 +1,11 @@
+/**
+在无限的整数序列 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...中找到第 n 个数字。
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+  if (n < 10) return n
+  
+};
