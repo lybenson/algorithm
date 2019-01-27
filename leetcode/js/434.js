187a28db7b7be0cbc5f6059e4d7c075dbc69ba14
lybenson <naivebenson@gmail.com>
2019-01-27 21:34:43 +0800

    434

---
+++ leetcode/js/434.js
@@ -0,0 +1,13 @@
+/**
+ * 统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+
+请注意，你可以假定字符串里不包括任何不可打印的字符。
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  if (!s) return 0
+  return s.split(' ').filter(x => x).length
+};
