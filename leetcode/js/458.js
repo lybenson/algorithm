c1c6d02996370e81b9c09aaec1bb934b46ba8d8f
lybenson <naivebenson@gmail.com>
2019-02-05 17:07:16 +0800

    458

---
+++ leetcode/js/458.js
@@ -0,0 +1,16 @@
+/**
+ * 有1000只水桶，其中有且只有一桶装的含有毒药，其余装的都是水。它们从外观看起来都一样。如果小猪喝了毒药，它会在15分钟内死去。
+
+问题来了，如果需要你在一小时内，弄清楚哪只水桶含有毒药，你最少需要多少只猪？
+ */
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function(buckets, minutesToDie, minutesToTest) {
+  let decimal  = minutesToTest / minutesToDie + 1
+  let ans = Math.ceil(Math.log(buckets) / Math.log(decimal))
+  return ans
+};
