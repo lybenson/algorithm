8a0a3a514518e8e66e3578978c221f156fbe526d
lybenson <naivebenson@gmail.com>
2019-02-05 18:49:05 +0800

    482

---
+++ leetcode/js/482.js
@@ -0,0 +1,19 @@
+/**
+ * 给定一个密钥字符串S，只包含字母，数字以及 '-'（破折号）。N 个 '-' 将字符串分成了 N+1 组。给定一个数字 K，重新格式化字符串，除了第一个分组以外，每个分组要包含 K 个字符，第一个分组至少要包含 1 个字符。两个分组之间用 '-'（破折号）隔开，并且将所有的小写字母转换为大写字母。
+
+给定非空字符串 S 和数字 K，按照上面描述的规则进行格式化。
+ */
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+  S = S.toLocaleUpperCase().replace(/\-/g, '')
+  if (S.length < K) return S
+  let len = S.length
+  for (let i = K; i < len; i += K) {
+    S = S.slice(0, len - i) + '-' + S.slice(len - i)
+  }
+  return S
+};
