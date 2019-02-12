7353c3c4cd70e0a3af578e02d99b468e4aadd7aa
lybenson <naivebenson@gmail.com>
2019-02-13 01:09:07 +0800

    141

---
+++ leetcode/js/141.js
@@ -0,0 +1,29 @@
+/**
+ * 给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if (head == null || head.next == null) return false
+
+  let slow = head
+  let fast = head.next
+  while (slow && fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) return true
+  }
+  return false
+};
