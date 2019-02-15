3364326918a9d6b130a6a8f07e11c040382e3ecc
lybenson <naivebenson@gmail.com>
2019-02-16 00:44:23 +0800

    203

---
+++ leetcode/js/203.js
@@ -0,0 +1,28 @@
+/**
+ * 删除链表中等于给定值 val 的所有节点。
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  let anotherHead = new ListNode(-1)
+  anotherHead.next = head
+  let node = anotherHead
+  while (node.next) {
+    if (node.next.val === val) {
+      node.next = node.next.next
+    } else {
+      node = node.next
+    }
+  }
+  return anotherHead.next
+};
