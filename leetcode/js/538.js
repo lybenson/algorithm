165df71a3a126daf1cd0cdd26314bc7a1451d106
lybenson <naivebenson@gmail.com>
2019-02-19 00:24:52 +0800

    538

---
+++ leetcode/js/538.js
@@ -0,0 +1,27 @@
+/**
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+  let sum = 0
+  function traverse(node) {
+    if (!node) return
+    traverse(node.right)
+    node.val += sum
+    sum = node.val
+    traverse(node.left)
+  }
+  traverse(root)
+  return root
+};
