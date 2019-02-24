b0f49dbb581a697e8ba6f8f05b977cd381fd3c9e
lybenson <naivebenson@gmail.com>
2019-02-24 17:26:05 +0800

    700

---
+++ leetcode/js/700.js
@@ -0,0 +1,28 @@
+/**
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+  let node = root
+  while (node) {
+    if (node.val < val) {
+      node = node.right
+    } else if (node.val > val) {
+      node = node.left
+    } else {
+      return node
+    }
+  }
+  return null
+};
