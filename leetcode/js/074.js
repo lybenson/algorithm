e76a1a6a8adbbb02f92e5568bc0491fe2098ad6b
lybenson <naivebenson@gmail.com>
2019-04-08 23:04:51 +0800

    154

---
+++ leetcode/js/074.js
@@ -0,0 +1,18 @@
+/**
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+每行中的整数从左到右按升序排列。
+每行的第一个整数大于前一行的最后一个整数。
+ */
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  let left = 0
+  let right = matrix.length - 1
+  while (left < right) {
+    
+  }
+};
