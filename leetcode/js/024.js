749c20436d253c4fe5647748f61f01601ad8496e
lybenson <naivebenson@gmail.com>
2019-04-24 22:58:04 +0800

    24

---
+++ leetcode/js/024.js
@@ -0,0 +1,23 @@
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if (!head || !head.next) return head
+  let next = head.next
+  head.next = swapPairs(next.next)
+  next.next = head
+  return next
+};
