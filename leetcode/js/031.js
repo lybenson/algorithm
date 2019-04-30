b929cdd6f3cfd375d2bbc1aed02b856e9fd84fe8
lybenson <naivebenson@gmail.com>
2019-04-30 23:59:34 +0800

    31

---
+++ leetcode/js/031.js
@@ -0,0 +1,52 @@
+/**
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+必须原地修改，只允许使用额外常数空间。
+
+以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+  if (!nums || nums.length === 0) return nums
+  let smallPos = -1
+  let len = nums.length
+  for (let i = len - 2; i < nums.length; i++) {
+    if (nums[i] < nums[i + 1]) {
+      smallPos = i
+      break
+    }
+  }
+  if (smallPos === -1) reverse(nums, 0, nums.length - 1)
+
+  let largePos = -1
+  for (let i = len - 1; i > smallPos; i--) {
+    if (nums[i] > nums[smallPos]) {
+      largePos = i
+      break
+    }
+  }
+  swap(nums, smallPos, largePos)
+  reverse(nums, smallPos + 1, nums.length - 1)
+
+  return nums
+};
+
+function reverse (nums, i, j) {
+  while (i < j) {
+    swap(nums, i++, j--)
+  }
+}
+function swap(nums, i, j) {
+  let temp = nums[i]
+  nums[i++] = nums[j]
+  nums[j--] = temp
+}
