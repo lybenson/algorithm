5a4d74e90726caa885708c7d5cdddda6f8cd0c0b
lybenson <naivebenson@gmail.com>
2019-08-27 23:59:17 +0800

    64

--- leetcode/064.js
+++ leetcode/064.js
@@ -13,15 +13,10 @@ var minPathSum = function (grid) {
   let n = grid[0].length
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      if (i === 0 && j === 0) {
-
-      } else if (i === 0) {
-        grid[i][j] += grid[i][j - 1]
-      } else if (j === 0) {
-        grid[i][j] += grid[i-1][j]
-      }else{
-        grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1])
-      }
+      if (i === 0 && j === 0) continue
+      else if (i === 0) grid[i][j] += grid[i][j - 1]
+      else if (j === 0) grid[i][j] += grid[i-1][j]
+      else grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1])
     }
   }
   return grid[m-1][n-1]
