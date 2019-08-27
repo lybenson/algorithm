077f2abe7a206ff3193360bdc1c78c66448974a0
lybenson <naivebenson@gmail.com>
2019-08-28 00:17:47 +0800

    62

--- leetcode/062.js
+++ leetcode/062.js
@@ -12,13 +12,13 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-  const dp = Array(n).fill(1);
-  
-  for(let i = 1; i < m; i++) {
-    for(let j = 1; j < n; j++) {
-      dp[j] = dp[j] + dp[j - 1];
-    } 
-  }
+  let inner = new Array(n).fill(1)
+  let dp = new Array(m).fill(inner)
 
-  return dp[n - 1];
+  for (let i = 1; i < m; i++) {
+    for(let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+  return dp[m - 1][n - 1]
 }
