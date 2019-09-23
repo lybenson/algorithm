c8f3aa64f17c57f6055af79cef64bc874e6e7635
lybenson <naivebenson@gmail.com>
2019-09-24 00:03:33 +0800

    876

---
+++ leetcode/876.js
@@ -0,0 +1,25 @@
+/**
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let slow = head
+  let fast = head
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+  return slow
+};
