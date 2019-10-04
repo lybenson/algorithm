fc1a5d94bd59f0dfb088cdd54405f5e1cca237ca
lybenson <naivebenson@gmail.com>
2019-10-04 21:28:22 +0800

    142

---
+++ leetcode/142.js
@@ -0,0 +1,29 @@
+/**
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+说明：不允许修改给定的链表。
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+  let set = new Set()
+  let node = head
+  while (node) {
+    if (set.has(node)) return node
+    set.add(node)
+    node = node.next
+  }
+  return null
+};
