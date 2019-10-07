66dc796578673665a2bbf286f74bc4c77f8e0706
lybenson <naivebenson@gmail.com>
2019-10-07 17:36:42 +0800

    328

---
+++ leetcode/328.js
@@ -0,0 +1,31 @@
+/**
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+
+请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+  if (!head) return null
+  let odd = head
+  let even = head.next
+  let evenHead = even
+  while (even && even.next) {
+    odd.next = even.next
+    odd = odd.next
+
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+  return head
+};
