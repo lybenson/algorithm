23f47e67fe4a5e85abaf73b892378cc05e4a2d6e
lybenson <naivebenson@gmail.com>
2019-10-25 22:28:38 +0800

    37

---
+++ leetcode/037.js
@@ -0,0 +1,18 @@
+/**
+编写一个程序，通过已填充的空格来解决数独问题。
+
+一个数独的解法需遵循如下规则：
+
+数字 1-9 在每一行只能出现一次。
+数字 1-9 在每一列只能出现一次。
+数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ */
+
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function(board) {
+  let m = board.length
+  let n = board[0].length
+};
