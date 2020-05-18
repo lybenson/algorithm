73d81a842840d6591531234dfacebe6b533590c2
lybenson <naivebenson@gmail.com>
2020-05-18 23:53:15 +0800

    215

--- leetcode/215.js
+++ leetcode/215.js
@@ -7,10 +7,43 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
+function swap(nums, i, j) {
+  const tmp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = tmp;
+}
 
-};
+function partition(nums, low, high) {
+  const pivot = nums[high];
 
-function buildMaxHeap () {
+  let middle = low;
+  for (let i = low; i < high; i++) {
+    if (nums[i] > pivot) {
+      swap(nums, i, middle);
+      middle += 1;
+    }
+  }
 
+  swap(nums, middle, high);
+
+  return middle;
+}
+
+function helper(nums, low, high, k) {
+  const m = partition(nums, low, high);
+  if (m === k) {
+    return nums[m];
+  } else if (m < k) {
+    return helper(nums, m + 1, high, k);
+  } else {
+    return helper(nums, low, m - 1, k);
+  }
+}
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  return helper(nums, 0, nums.length - 1, k - 1);
 }
