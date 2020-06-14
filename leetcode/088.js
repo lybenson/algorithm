4ebc5564602bf67217b6d37a7a388d57a94f4060
lybenson <naivebenson@gmail.com>
2020-06-14 11:13:49 +0800

    88

--- leetcode/088.js
+++ leetcode/088.js
@@ -9,12 +9,16 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-  nums1.length = m
-  nums2.length = n
-
-  Array.prototype.push.apply(nums1, nums2)
-  
-  nums1.sort((a, b) => {
-    return a - b
-  })
+  let p1 = m - 1
+  let p2 = n - 1
+  let p = (m + n) - 1
+  while (p2 >= 0) {
+    if (p1 < 0) {
+      nums1[p--] = nums2[p2--]
+      continue
+    }
+    nums1[p--] = nums1[p1] >= nums2[p2] ? nums1[p1--]: nums2[p2--]
+  }
 }
+
+merge([1,2,3,0,0,0], 3, [2,5,6], 3)
