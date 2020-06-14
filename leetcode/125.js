ddba016be11589fb8aeb52531d38e9b9a2b98b37
lybenson <naivebenson@gmail.com>
2020-06-14 12:12:15 +0800

    125

--- leetcode/125.js
+++ leetcode/125.js
@@ -6,8 +6,36 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  s = s.replace(/[\W]/g, '')
-  s = s.toLowerCase()
-  let newS = s.split('').reverse().join('')
-  return s === newS
+  s = s.toLocaleLowerCase()
+
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    if (!isValid(s[left])) {
+      left++
+      continue
+    }
+
+    if (!isValid(s[right])) {
+      right--
+      continue
+    }
+    if (s[left] === s[right]) {
+      left++
+      right--
+    } else {
+      break
+    }
+  }
+
+  return right <= left
 };
+
+function isValid (char) {
+  let charCode = char.charCodeAt(0)
+  let isDigit = charCode >= '0'.charCodeAt(0) && charCode <= '9'.charCodeAt(0)
+  let isChar = charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0)
+
+  return isDigit || isChar
+}
