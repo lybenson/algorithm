bb1a78a690429bdbf75e59f6d6d04cba71bc9cde
lybenson <naivebenson@gmail.com>
2020-06-16 21:44:25 +0800

    1143

---
+++ leetcode/1143.js
@@ -0,0 +1,37 @@
+/**
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+
+若这两个字符串没有公共子序列，则返回 0。
+ */
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+  let m = text1.length
+  let n = text2.length
+
+  // 区别？
+  // let dp = new Array(m + 1).fill(new Array(n + 1).fill(0))
+  let dp = Array.from(new Array(m + 1),() => new Array(n + 1).fill(0))
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1]
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[m][n]
+}
+
+let x = longestCommonSubsequence("pmjghexybyrgzczy", "hafcdqbgncrcbihkd")
+console.log(x)
+
