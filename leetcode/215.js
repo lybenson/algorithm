943227e725a89b5633a42912f86d670d9b9759e5
lybenson <naivebenson@gmail.com>
2020-07-04 19:05:47 +0800

    215

--- leetcode/215.js
+++ leetcode/215.js
@@ -51,67 +51,122 @@
 
 // --------------------maxheap--------------------
 
+// function swap (arr, i, j) {
+//   const temp = arr[i]
+//   arr[i] = arr[j]
+//   arr[j] = temp
+// }
+
+// function heapify (arr, n, i) {
+//   if (i >= n) return
+//   const left_index = 2 * i + 1
+//   const right_index = 2 * i + 2
+
+//   let max_index = i
+//   if (left_index < n && arr[max_index] < arr[left_index]) {
+//     max_index = left_index
+//   }
+//   if (right_index < n && arr[max_index] < arr[right_index]) {
+//     max_index = right_index
+//   }
+//   // console.log('max-index=' + max_index);
+  
+//   if (max_index !== i) {
+//     swap(arr, i, max_index)
+//     heapify(arr, n, max_index)
+//   }
+// }
+
+// function buildHeap (array) {
+//   let length = array.length
+//   let lastIndex = length - 1
+//   let parent = ~~((lastIndex - 1) / 2)
+
+//   for (let i = parent; i >= 0; i--) {
+//     heapify(array, length, i)
+//   }
+// }
+
+// function heap_sort (array, n) {
+//   buildHeap(array, n)
+
+//   for (let i = n - 1; i >= 0; i--) {
+//     console.log(array)
+//     swap(array, i, 0)
+//     console.log(array);
+//     console.log('----')
+//     heapify(array, i, 0)
+//   }
+// }
+
+// var findKthLargest = function(nums, k) {
+//   // buildHeap(nums)
+//   // heapify(nums, nums.length, 0)
+//   heap_sort(nums, nums.length)
+//   console.log(nums)
+//   // for (let i = 0; i < k - 1; i++) {
+//   //   nums.shift()
+
+//   //   heap_sort(nums, nums.length)
+//   // }
+
+//   // console.log(nums)
+//   // return nums[0]
+// };
+
+// findKthLargest([5,2,4,1,3,6,0], 2)
+
+var findKthLargest = function(nums, k) {
+  let minHeap = nums.slice(0, k)
+  console.log(minHeap)
+  buildHeap(minHeap)
+
+  console.log(minHeap)
+
+  for (let i = k; i < nums.length; i++) {
+    if (minHeap[0] < nums[i]) {
+      minHeap[0] = nums[i]
+      heapify(minHeap, k, 0)
+    }
+  }
+  
+  return minHeap[0]
+}
+
+// 假设某一元素的索引是index
+// 则其父节点的索引为 (index - 1) / 2
+// 左节点的索引为 2 * index + 1
+// 右节点的索引为 2 * index + 2
+function buildHeap (array) {
+  let len = array.length
+  let lastIndex = len - 1
+
+  let parentIndex = ~~((lastIndex - 1) / 2)
+  for (let i = parentIndex; i >= 0; i-- ) {
+    heapify(array, len, i)
+  }
+}
+
+function heapify (array, length, index) {
+  let leftIndex = 2 * index + 1
+  let rightIndex = 2 * index + 2
+
+  let min = index
+  if (leftIndex < length && array[leftIndex] < array[min]) min = leftIndex
+  if (rightIndex < length && array[rightIndex] < array[min]) min = rightIndex
+
+  if (min !== index) {
+    swap(array, min, index)
+
+    heapify(array, length, min)
+  }
+}
+
 function swap (arr, i, j) {
   const temp = arr[i]
   arr[i] = arr[j]
   arr[j] = temp
 }
 
-function heapify (arr, n, i) {
-  if (i >= n) return
-  const left_index = 2 * i + 1
-  const right_index = 2 * i + 2
-
-  let max_index = i
-  if (left_index < n && arr[max_index] < arr[left_index]) {
-    max_index = left_index
-  }
-  if (right_index < n && arr[max_index] < arr[right_index]) {
-    max_index = right_index
-  }
-  // console.log('max-index=' + max_index);
-  
-  if (max_index !== i) {
-    swap(arr, i, max_index)
-    heapify(arr, n, max_index)
-  }
-}
-
-function buildHeap (array) {
-  let length = array.length
-  let lastIndex = length - 1
-  let parent = ~~((lastIndex - 1) / 2)
-
-  for (let i = parent; i >= 0; i--) {
-    heapify(array, length, i)
-  }
-}
-
-function heap_sort (array, n) {
-  buildHeap(array, n)
-
-  for (let i = n - 1; i >= 0; i--) {
-    console.log(array)
-    swap(array, i, 0)
-    console.log(array);
-    console.log('----')
-    heapify(array, i, 0)
-  }
-}
-
-var findKthLargest = function(nums, k) {
-  // buildHeap(nums)
-  // heapify(nums, nums.length, 0)
-  heap_sort(nums, nums.length)
-  console.log(nums)
-  // for (let i = 0; i < k - 1; i++) {
-  //   nums.shift()
-
-  //   heap_sort(nums, nums.length)
-  // }
-
-  // console.log(nums)
-  // return nums[0]
-};
-
-findKthLargest([5,2,4,1,3,6,0], 2)
+let res = findKthLargest([7,6,5,4,3,2,1],5)
+console.log(res);
