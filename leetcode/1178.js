84428fd9ca333dff3a9adf00464c59368a7552a6
lybenson <naivebenson@gmail.com>
2021-03-05 21:52:06 +0800

    1178

--- leetcode/1178.js
+++ leetcode/1178.js
@@ -15,5 +15,51 @@
  * @return {number[]}
  */
 var findNumOfValidWords = function(words, puzzles) {
+  const frequency = new Map()
 
-};
+  for (const word of words) {
+    let mask = 0
+    for (const ch of word) {
+      mask |= (1 << (ch.charCodeAt() - 'a'.charCodeAt()))
+    }
+    if (CountOne(mask) <= 7) {
+      frequency.set(mask, (frequency.get(mask) || 0) + 1)
+    }
+  }
+
+  const ans = []
+  for (const puzzle of puzzles) {
+    let total = 0
+
+    let mask = 0
+    for (let i = 1; i < 7; ++i) {
+        mask |= (1 << (puzzle[i].charCodeAt() - 'a'.charCodeAt()))
+    }
+    let subset = mask
+    while (subset) {
+      let s = subset | (1 << (puzzle[0].charCodeAt() - 'a'.charCodeAt()))
+      if (frequency.has(s)) {
+        total += frequency.get(s)
+      }
+      subset = (subset - 1) & mask
+    }
+    // 在枚举子集的过程中，要么会漏掉全集 mask，要么会漏掉空集
+    // 这里会漏掉空集，因此需要额外判断空集
+    if (frequency.has(1 << (puzzle[0].charCodeAt() - 'a'.charCodeAt()))) {
+      total += frequency.get(1 << (puzzle[0].charCodeAt() - 'a'.charCodeAt()))
+    }
+    ans.push(total)
+  }
+  return ans
+}
+
+function CountOne(n) {
+  const str = n.toString(2)
+  let count = 0
+  for (const ch of str) {
+    if (parseInt(ch) === 1) {
+      count++
+    }
+  }
+  return count
+}
