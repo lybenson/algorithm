fd37fd8a6482d67934747be2051a890e7b955d81
benson <757766483@qq.com>
2022-05-24 14:48:49 +0800

    Update 21_调整数组顺序使奇数位于偶数前面.js

--- swordoffer/21_调整数组顺序使奇数位于偶数前面.js
+++ swordoffer/21_调整数组顺序使奇数位于偶数前面.js
@@ -1,3 +1,20 @@
 /**
 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
  */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var exchange = function(nums) {
+  let start = 0
+  let end = nums.length - 1
+
+  while(start < end) {
+    while (start < end && nums[start] & 1 === 1) i += 1
+    while (start < end && nums[end] & 1 === 0) end -= 1
+
+    [nums[start], nums[end]] = [nums[end], nums[start]]
+  }
+  return nums
+};
